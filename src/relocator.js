b9be06346fdae77fbc183c547cafc22f598d8ff9
overtrue <anzhengchao@gmail.com>
2015-09-21 13:57:11 +0800

    Bump version 1.0.2

--- src/relocator.js
+++ src/relocator.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview relocator.js
- * @version 1.0.1
+ * @version 1.0.2
  *
  * @author overtrue <anzhengchao@gmail.com>
  * @see    https://github.com/overtrue
@@ -14,19 +14,10 @@
 
 /**
  * See (http://jquery.com/).
- * @name $
+ * @name relocator
  * @class
  * See the jQuery Library  (http://jquery.com/) for full details.  This just
  * documents the function and classes that are added to jQuery by this plug-in.
- */
-
-/**
- * See (http://jquery.com/)
- * @name fn
- * @class
- * See the jQuery Library  (http://jquery.com/) for full details.  This just
- * documents the function and classes that are added to jQuery by this plug-in.
- * @memberOf $
  */
 ;(function (factory) {
     if (typeof define === 'function' && define.amd && define.amd.jQuery) {
@@ -37,33 +28,32 @@
         factory(jQuery);
     }
 }(function ($) {
-    $.fn.relocate = function(box_selector) {
+    $.fn.relocate = function(boxSelector) {
         this.each(function() {
             var img     = $(this);
-            var box     = box_selector ? img.closest(box_selector) : img.parent();
-            var tmp_img = new Image();
+            var box     = boxSelector ? img.closest(boxSelector) : img.parent();
+            var tmpImg = new Image();
 
             box.css({'position':'relative', 'overflow':'hidden', 'max-width':'auto'});
 
-            tmp_img.src = img.attr('src');
+            tmpImg.src = img.attr('src');
 
-            $(tmp_img).load(function() {
+            $(tmpImg).load(function() {
                 var tmp = $(this).get(0);
-                var img_real_width  = tmp.naturalWidth;
-                var img_real_height = tmp.naturalHeight;
+                var imgRealWidth  = tmp.naturalWidth;
+                var imgRealHeight = tmp.naturalHeight;
 
-                var box_width  = parseInt(box.css('width'));
-                var box_height = parseInt(box.css('height'));
+                var boxWidth  = parseInt(box.css('width'));
+                var boxHeight = parseInt(box.css('height'));
 
-                var img_ratio = img_real_width / img_real_height;
-                var box_ratio = box_width / box_height;
+                var imgRatio = imgRealWidth / imgRealHeight;
 
-                if (img_ratio < box_ratio) {
-                   var top_distance = (((box_width / img_real_width) * img_real_height) - box_height) / 2;
-                   img.css({'width':box_width, 'height':'auto', 'margin-top':-top_distance});
+                if (imgRatio < 1) {
+                   var topDistance = (((boxWidth / imgRealWidth) * imgRealHeight) - boxHeight) / 2;
+                   img.css({'width':boxWidth, 'height':'auto', 'max-height':'none', 'margin-top':-topDistance});
                 } else {
-                   var left_distance = (((box_height / img_real_height) * img_real_width) - box_width) / 2;
-                   img.css({'width':'auto', 'height':box_height, 'margin-left':-left_distance});
+                   var leftDistance = (((boxHeight / imgRealHeight) * imgRealWidth) - boxWidth) / 2;
+                   img.css({'width':'auto', 'max-width':'none',  'height':boxHeight, 'margin-left':-leftDistance});
                 }
             });
         });
